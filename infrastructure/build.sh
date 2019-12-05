43eab8728e6c894e39771f7365f96121d69f35fd
Lucas Santos <lhs.santoss@gmail.com>
2019-12-05 12:59:34 -0300

    Change registry location

--- infrastructure/build.sh
+++ infrastructure/build.sh
@@ -1,7 +1,7 @@
 PARENT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && cd .. && pwd )
 PACKAGE_VERSION=$(node -pe "require('$PARENT_DIR/package.json').version")
 PACKAGE_NAME=$(node -pe "require('$PARENT_DIR/package.json').name")
-IMAGE_TAG=nindooai.azurecr.io/$PACKAGE_NAME:v${1:-$PACKAGE_VERSION}
+IMAGE_TAG=chegaai/$PACKAGE_NAME:v${1:-$PACKAGE_VERSION}
 
 read -p "Building and publishing $IMAGE_TAG. Press [enter] to continue..."
 
